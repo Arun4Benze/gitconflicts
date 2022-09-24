f3e81e36f24fca15894458a216398f2aa3f3479a
Arun4Benze <arunbenz93@gmail.com>
2022-09-24 08:55:31 +0530

    benz branch commit

--- src/com/gitconflict/Sample.java
+++ src/com/gitconflict/Sample.java
@@ -3,7 +3,8 @@ package com.gitconflict;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("master branch");
-		System.out.println("Arun branch");
+		System.out.println("new message");
+		System.out.println("Benze branch");
 	}
 
 }
